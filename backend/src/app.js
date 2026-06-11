b8b0d7dfa152c09a5be229787e89d31aca658d79
Jorsz <jordanjosia929@gmail.com>
2026-06-11 12:17:54 +0800

    tes

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use("/api", adminDataRoutes);
 // Serve React frontend
 const buildPath = path.join(__dirname, "../../client/build");
 app.use(express.static(buildPath));
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
